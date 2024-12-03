3996cc8416e46d1b00bbb374365b34281522934b
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2024-12-03 18:13:38 -0500

    working on metrics.py

--- hr_monitoring_data_processing/metrics.py
+++ hr_monitoring_data_processing/metrics.py
@@ -8,8 +8,19 @@ def window_max(data: list, n: int) -> list:
     Returns:
         list[int]: list of maximums from each window (size should be len(data)//6)
     """
+
     maximums = []
-    ...
+    max = 0
+    for num in range(n):
+        for num in data:
+           if num > max:
+               max = num  
+           maximums.append(max)     
+        return maximums
+        
+print(window_max([1, 2, 3, 4, 5, 6, 7], 1))
+
+... 
 
 
 def window_average(data: list, n: int) -> list:
