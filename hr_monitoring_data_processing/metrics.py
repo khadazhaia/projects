14568e1935bebf02d2c6dec2e4e8f14460dd55bb
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2024-12-04 21:45:00 -0500

    used start:stop:skip

--- hr_monitoring_data_processing/metrics.py
+++ hr_monitoring_data_processing/metrics.py
@@ -8,17 +8,30 @@ def window_max(data: list, n: int) -> list:
     Returns:
         list[int]: list of maximums from each window (size should be len(data)//6)
     """
+     
+    '''Rolling Average 
 
-    maximums = []
-    max = 0
-    for num in range(n):
-        for num in data:
-           if num > max:
-               max = num  
-           maximums.append(max)     
-        return maximums
+[1, 3, 4, 2, 8, 10 ,12]
+
+N = 2 (WINDOW SIZE EVERY 2 ELEMENTS)
+
+
+This outputs the average from every 2
+So average of 
+
+[1, 3], [4, 2], [8, 10], 12
+[2, 3, 9, 12]'''
+    
+ 
+    
         
-print(window_max([1, 2, 3, 4, 5, 6, 7], 1))
+    maximum = []
+    for wind in range(1):
+        wind = data[::n]
+        maximum.append(((wind)))    
+    return maximum
+        
+print(window_max([1, 2, 3, 4, 5, 6, 7], 4))
 
 ... 
 
