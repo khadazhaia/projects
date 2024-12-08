9cee17456601f129ee8c7acc91f0290a818fc839
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2024-12-08 13:19:43 -0500

    window_max passes all tests

--- hr_monitoring_data_processing/metrics.py
+++ hr_monitoring_data_processing/metrics.py
@@ -22,17 +22,21 @@ So average of
 [1, 3], [4, 2], [8, 10], 12
 [2, 3, 9, 12]'''
     
- 
+#  Breakdown of the formula:
+# SMAj: Represents the Simple Moving Average at position "j" in the data series.
+# (1/k): This part simply divides the sum by the number of data points considered ("k") to calculate the average.
+# ∑ (i=j-1 to j+k-1) ai:
+# ∑: This symbol represents the "summation" operation, meaning we are adding up a series of values.
+# i=j-1 to j+k-1: This indicates that we are summing up values from position "j-1" (one point before "j") to "j+k-1" (k points after "j"), effectively including "k" data points centered around "j".
+# ai: Represents the value at position "i" in the data series
     
-        
+    window_size = n    
     maximum = []
-    for wind in range(1):
-        wind = data[::n]
-        maximum.append(((wind)))    
+    for num in range(0, len(data), window_size):
+         window = data[num:num + window_size]
+         maximum.append(max(window))
     return maximum
-        
-print(window_max([1, 2, 3, 4, 5, 6, 7], 4))
-
+    
 ... 
 
 
