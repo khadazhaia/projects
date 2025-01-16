c1a62194a462a6e5172566148c17374d598c3d86
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2025-01-15 19:17:25 -0500

    updated weather_api code

--- weather_api/weather_api.py
+++ weather_api/weather_api.py
@@ -20,9 +20,19 @@ with open(file_path, "w") as file:
 
 # Remove all the meta-data from this resultant JSON file and keep only the data that describes the time and the variables listed above. Convert this modified JSON file into a CSV file and save it to the path `data/CSV/`. 
 
+
+# filtered_data = pd.DataFrame({
+#     "time": df["time"].iloc[0],  
+#     "temperature": df["temperature"].iloc[0],
+#     "relative humidity": df["relative humidity"].iloc[0],
+#     "precipitation": df["precipitation"].iloc[0],
+#     "surface pressure": df["surface pressure"].iloc[0]
+# })
+
 df = pd.DataFrame(data)
 
-filtered_data = df.drop(columns = ["latitude", "longitude","generationtime_ms","utc_offset_seconds","timezone","timezone_abbreviation","elevation","hourly_units"])
+columns_to_keep = ["time", "temperature_2m", "relative_humidity_2m", "precipitation", "surface_pressure"]
+filtered_data = pd.DataFrame(data["hourly"])[columns_to_keep]
 
 filtered_data.to_csv("data/csv/data.csv", index=False)
 
