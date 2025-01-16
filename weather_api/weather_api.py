eb31fe702bd662735f3924a13e95d28ed694ce71
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2025-01-15 19:58:37 -0500

    MG yearly medians

--- weather_api/weather_api.py
+++ weather_api/weather_api.py
@@ -20,19 +20,13 @@ with open(file_path, "w") as file:
 
 # Remove all the meta-data from this resultant JSON file and keep only the data that describes the time and the variables listed above. Convert this modified JSON file into a CSV file and save it to the path `data/CSV/`. 
 
+df = pd.DataFrame({
+    "time": data["hourly"]["time"],
+    "temperature": data["hourly"]["temperature_2m"],
+    "relative humidity": data["hourly"]["relative_humidity_2m"],
+    "precipitation": data["hourly"]["precipitation"],
+    "surface pressure": data["hourly"]["surface_pressure"]
+})
 
-# filtered_data = pd.DataFrame({
-#     "time": df["time"].iloc[0],  
-#     "temperature": df["temperature"].iloc[0],
-#     "relative humidity": df["relative humidity"].iloc[0],
-#     "precipitation": df["precipitation"].iloc[0],
-#     "surface pressure": df["surface pressure"].iloc[0]
-# })
-
-df = pd.DataFrame(data)
-
-columns_to_keep = ["time", "temperature_2m", "relative_humidity_2m", "precipitation", "surface_pressure"]
-filtered_data = pd.DataFrame(data["hourly"])[columns_to_keep]
-
-filtered_data.to_csv("data/csv/data.csv", index=False)
+df.to_csv("data/csv/data.csv", index=False)
 
